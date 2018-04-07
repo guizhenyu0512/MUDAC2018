a9bca051bcc49f519c712089598b0395ea6f4503
guizhenyu0512 <grady0512@gmail.com>
2018-04-06 22:00:53 -0500

    Add files via upload

--- code_storage/linear_regression.R
+++ code_storage/linear_regression.R
@@ -1,2 +1,15 @@
 #建模型公式
 lm(DV ~ IV1 + IV2 + IV3, data = 导入数据集)
+
+#anova 分析
+anova()
+gaping
+#predict
+model_predict = predict(model, newdata = 数据集)
+
+#如何检测predict？ 
+#先算出sse（(prediction - real data set)^2）
+#再算sst ((mean - real data set)^2)
+#算R2  1- sse/sst
+
+#RMSE (root mean squared error)  sqrt(SSE/total # of data points)
--- code_storage/通用函数.R
+++ code_storage/通用函数.R
@@ -26,11 +26,27 @@ sum(variable1)
 #计算correlation
 cor(variable1, variable2)
 
+#找出总数据点数
+nrow(data)
+
 #表格列出频度, variable1 为列， variable2 为行
 table(variable1, variable2)
+
+#设定种子
+set.seed(整数)
 
 #将variable 按比例分出true， 其他为false
 sample.split(outcome variable, % in trainingset)
 
 #取subset, 第二处还可用上面的函数。 var = sample.split(```), var = true
 subset(data, variable < n )
+
+
+## 画图类 ##
+
+#basic画图
+plot(IV, DV)
+
+#画一组图
+pair(DV ~ IV1 + IV2, data = 数据集)
+
